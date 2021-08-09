3f3feeb11cf399725acb509058e7db2a9883f39d
Nipun Gupta <nipun.gupta2@gmail.com>
2021-08-10 01:06:31 +0530

    React app with team page

--- src/main/java/com/nipun/ipldashboard/controller/TeamController.java
+++ src/main/java/com/nipun/ipldashboard/controller/TeamController.java
@@ -7,10 +7,12 @@ import com.nipun.ipldashboard.model.Team;
 import com.nipun.ipldashboard.repository.MatchRepository;
 import com.nipun.ipldashboard.repository.TeamRepository;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class TeamController {
 
