36ea1c7d115c8c48c9e60208907437fb3bd23771
Nipun Gupta <nipun.gupta2@gmail.com>
2021-08-29 01:52:45 +0530

    make app production ready

--- src/frontend/src/pages/HomePage.js
+++ src/frontend/src/pages/HomePage.js
@@ -12,7 +12,7 @@ export const HomePage = () => {
     useEffect(
         () => {
             const fetchAllTeams = async () => {
-                const response = await fetch('http://localhost:8080/teams');
+                const response = await fetch(`${process.env.REACT_APP_API_ROOT_URL}/teams`);
                 const data = await response.json();
                 setTeams(data);
             };
